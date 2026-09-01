64514e28855b74cd3a145cabceb00d1eaaea059f
MelinaRajabi <129985600+meeeeliiiinaaaa@users.noreply.github.com>
2026-09-01 17:16:04 +0330

    Delete calpro-github-ready/calpro-repo directory

--- calpro-github-ready/calpro-repo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.calpro {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.calpro to javafx.fxml;
-    exports com.example.calpro;
-}
